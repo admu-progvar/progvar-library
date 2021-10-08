92824db9b2b67c8ab4812df842909515ecccaabf
leloykun <franzlouiscesista@gmail.com>
2021-10-09 01:35:22 +0800

    add tree diameter and centers

---
+++ pvl/base.hpp
@@ -0,0 +1,20 @@
+#ifndef PVL_BASE_HPP_
+#define PVL_BASE_HPP_ 1
+
+#include <vector>
+
+namespace pvl {
+
+typedef long long ll;
+
+typedef std::vector<int> vi;
+typedef std::vector<vi> vvi;
+
+typedef std::vector<ll> vll;
+
+std::pair<int, int> pii;
+std::pair<ll, ll> pll;
+
+}  // namespace pvl
+
+#endif  // PVL_BASE_HPP_
---
+++ pvl/graphs/tree/tree_diameter.hpp
@@ -0,0 +1,27 @@
+#ifndef PVL_GRAPHS_TREE_TREE_DIAMETER_HPP_
+#define PVL_GRAPHS_TREE_TREE_DIAMETER_HPP_ 1
+
+#include "base.hpp"
+#include "graphs/unweighted_graphs.hpp"
+#include "graphs/basic_traversal/bfs.hpp"
+
+
+namespace pvl {
+
+vi find_tree_diameter(const graph_adj_list &g) {
+  /*
+    Finds a diamter path of a tree g.
+
+    Assumption: g is a tree.
+  */
+  bfs_data data_1 = bfs(g, 0);
+  bfs_data data_2 = bfs(g, data_1.farthest);
+  vi diameter;
+  for (int u = data_2.farthest; u != -1; u = data_2.par[u])
+    diameter.push_back(u);
+  return diameter;
+}
+
+}  // namespace pvl
+
+#endif  // PVL_GRAPHS_TREE_TREE_DIAMETER_HPP_
