df4355a4848512cae0354ef67714baf29b7332a3
billybutcher <3123justin@gmail.com>
2022-03-24 01:29:07 +0800

    team name can be passed as argument

--- notebook/makefile.sh
+++ notebook/makefile.sh
@@ -1,12 +1,19 @@
+while getopts t: flag
+do
+    case "${flag}" in
+        t) teamname=${OPTARG};;
+    esac
+done
+
 rm -f *.{aux,log,toc,fls,fdb_latexmk}
 python3 preprocess.py
 python3 build_tex.py
 
-pdflatex -shell-escape -jobname="notebook-trunc" notebook.tex
-pdflatex -shell-escape -jobname="notebook-trunc" notebook.tex
+pdflatex -shell-escape -jobname="notebook-trunc" "\def\ICPCCONFIG{1} \def\team{$teamname} \input{notebook.tex}"
+pdflatex -shell-escape -jobname="notebook-trunc" "\def\ICPCCONFIG{1} \def\team{$teamname} \input{notebook.tex}"
 
-pdflatex -shell-escape "\def\ICPCCONFIG{1} \input{notebook.tex}"
-pdflatex -shell-escape "\def\ICPCCONFIG{1} \input{notebook.tex}"
+pdflatex -shell-escape "\def\ICPCCONFIG{1} \def\team{$teamname} \input{notebook.tex}"
+pdflatex -shell-escape "\def\ICPCCONFIG{1} \def\team{$teamname} \input{notebook.tex}"
 
 rm -f *.{aux,log,toc,fls,fdb_latexmk}
 #rm -rf _code/
