d0b41f48a5655f85ff1c64d76e5b0659360699fb
Mark Kevin A. Ong Yiu <markkevin.ongyiu@gmail.com>
2022-03-30 11:20:38 +0800

    Added tests and compressed abridged

--- pvl/abridged/strings/z.cpp
+++ pvl/abridged/strings/z.cpp
@@ -4,6 +4,4 @@ void computeZ(string s) {
   for (int i = 0; i < n; i++) {
     if (i <= R) z[i] = min(R-i+1, z[i-L]);
     while(i+z[i] < n && s[z[i]] == s[i+z[i]]) z[i]++;
-    if (i+z[i]-1 > R) L = i, R = i+z[i]-1;
-  }
-}
+    if (i+z[i]-1 > R) L = i, R = i+z[i]-1; } }
---
+++ tests/z.cc
@@ -0,0 +1,42 @@
+#include <gtest/gtest.h>
+#include "../pvl/strings/z.hpp"
+
+TEST(ZAlgo, aaaaaa) {
+    std::string s = "aaaaaa";
+    int expected[] = {6, 5, 4, 3, 2, 1};
+    std::vector<int> z = pvl::computeZ(s);
+    int n = s.length();
+    for (int i = 0; i < n; i++) {
+        EXPECT_EQ(expected[i], z[i]);
+    }
+}
+
+TEST(ZAlgo, aabaacd) {
+    std::string s = "aabaacd";
+    int expected[] = {7, 1, 0, 2, 1, 0, 0};
+    std::vector<int> z = pvl::computeZ(s);
+    int n = s.length();
+    for (int i = 0; i < n; i++) {
+        EXPECT_EQ(expected[i], z[i]);
+    }
+}
+
+TEST(ZAlgo, abababab) {
+    std::string s = "abababab";
+    int expected[] = {8, 0, 6, 0, 4, 0, 2, 0};
+    std::vector<int> z = pvl::computeZ(s);
+    int n = s.length();
+    for (int i = 0; i < n; i++) {
+        EXPECT_EQ(expected[i], z[i]);
+    }
+}
+
+TEST(ZAlgo, aab$baabaa) {
+    std::string s = "aab$baabaa";
+    int expected[] = {10, 1, 0, 0, 0, 3, 1, 0, 2, 1};
+    std::vector<int> z = pvl::computeZ(s);
+    int n = s.length();
+    for (int i = 0; i < n; i++) {
+        EXPECT_EQ(expected[i], z[i]);
+    }
+}
