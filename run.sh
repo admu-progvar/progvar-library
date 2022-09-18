ce9ccb5bbfa3eb4387ea2e3f86041f1c069714b1
leloykun <franzlouiscesista@gmail.com>
2022-09-19 03:13:30 +0800

    fix interface

---
+++ run.sh
@@ -0,0 +1,21 @@
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+name=$1
+if [[ $name == "init" ]]; then
+    pip3 install -r $SCRIPT_DIR/requirements.txt
+    pip3 install -r $SCRIPT_DIR/requirements.devtools.txt
+    pre-commit install
+    cat $SCRIPT_DIR/requirements.system.txt | xargs sudo apt install;
+elif [[ $name == "test" ]]; then
+    cd $SCRIPT_DIR/tests
+    cmake -S $SCRIPT_DIR/tests -B $SCRIPT_DIR/tests/build
+    cmake -B $SCRIPT_DIR/tests/build
+    cd $SCRIPT_DIR/tests/build
+    ctest;
+elif [[ $name == "build-notebook" ]]; then
+    cd $SCRIPT_DIR/notebook
+    bash build_notebook.sh;
+else
+    echo "Command $name not found" >&2
+    exit 1
+fi
